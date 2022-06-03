9bef352c9f3c2968c1019c85843261ed30a524ad
Jundullah H. R <jundihanif77@gmail.com>
2022-06-03 18:09:23 +0700

    fix wrong port deploy

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { BearerDefault, BearerMlDefault } = require('./src/auth')
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.NODE_ENV === 'production'? 80 : 3000,
+    host: '0.0.0.0',
   })
 
   await server.register({
